d5fed4058aa52660408571df37c35da36d7fd5c7
Nahuel1414 <nahuelgarciavera1414@gmail.com>
2024-10-04 18:04:31 +0000

    Punto A

---
+++ RTA_SCRIPT_Examen_20241004/Punto_A.sh
@@ -0,0 +1,11 @@
+mkdir /Examenes-UTN/
+cd /Examenes-UTN/
+mkdir alumno_1 alumno_2 alumno_3 profesores
+cd alumno_1
+mkdir parcial_1 parcial_2 parcial_3
+cd ..
+cd alumno_2
+mkdir parcial_1 parcial_2 parcial_3
+cd ..
+cd alumno_3
+mkdir parcial_1 parcial_2 parcial_3
