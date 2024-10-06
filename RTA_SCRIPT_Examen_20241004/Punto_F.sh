033bc74a56d6f4d32848aa0a9299117647689171
Nahuel1414 <nahuelgarciavera1414@gmail.com>
2024-10-06 15:38:07 +0000

    Punto F

--- RTA_SCRIPT_Examen_20241004/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_F.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-grep -i "memtotal" /proc/meminfo >> ~/UTNFRA_SO_1P2C_2024_GarciaVera/RTA_Archivos_Examen_20241004/Filtro_Avanzado.txt
+
+echo "Mi IP publica es: $(curl -s ifconfig.me)" >> ~/UTNFRA_SO_1P2C_2024_GarciaVera/RTA_ARCHIVOS_Examen_20241004/Filtro_Avanzado.txt
+echo "Mi usuario es: $(whoami)" >> ~/UTNFRA_SO_1P2C_2024_GarciaVera/RTA_ARCHIVOS_Examen_20241004/Filtro_Avanzado.txt
+echo "El hash de mi usuario es: $(sudo grep vagrant /etc/shadow | awk -F ':' '{print $2}')" >> ~/UTNFRA_SO_1P2C_2024_GarciaVera/RTA_ARCHIVOS_Examen_20241004/Filtro_Avanzado.txt
+echo "La URL de mi repositorio es: $(git remote get-url origin)" >> ~/UTNFRA_SO_1P2C_2024_GarciaVera/RTA_ARCHIVOS_Examen_20241004/Filtro_Avanzado.txt
